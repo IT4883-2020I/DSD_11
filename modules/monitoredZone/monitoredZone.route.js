c76178c051f81b29007e0e86b0cbedc31fab6b27
nganluu <nganltt712@gmail.com>
2020-12-01 11:03:16 +0700

    unable delete

--- modules/monitoredZone/monitoredZone.route.js
+++ modules/monitoredZone/monitoredZone.route.js
@@ -7,7 +7,7 @@ router.get("/area/:_id", monitoredZoneController.getZonebyArea);
 router.get("/", monitoredZoneController.getAllZone);
 router.get("/:_id", monitoredZoneController.getZonebyId);
 router.post("/area/:_id", monitoredZoneController.createZone);
-router.delete("/:_id", monitoredZoneController.deleteZone)
+//router.delete("/:_id", monitoredZoneController.deleteZone)
 router.put("/:_id", monitoredZoneController.updateZone);
 router.get('/statisticFrequency/:freq',  monitoredZoneController.statisticFrequency);
 router.get('/statisticLevel/:level',  monitoredZoneController.statisticLevel);
