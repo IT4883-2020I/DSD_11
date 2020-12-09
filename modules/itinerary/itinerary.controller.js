7e159c2d38caba2c106d0a66412aec815f673f02
nganluu <nganltt712@gmail.com>
2020-12-09 18:01:57 +0700

    .

--- modules/itinerary/itinerary.controller.js
+++ modules/itinerary/itinerary.controller.js
@@ -1,19 +1,19 @@
-const droneService = require('./itinerary.service');
+const itineraryService = require('./itinerary.service');
 
 exports.setItinerarytoZone = async (req, res) => {
     try {
         let data = req.body;
-        let result = await droneService.setItinerarytoZone(data);
+        let result = await itineraryService.setItinerarytoZone(data);
         res.status(200).json({
             success: true,
-            message: "Set drone to zone successfully",
+            message: "Set itinerary to zone successfully",
             content: result
         })
 
     }catch(error){
         res.status(400).json({
             success: false, 
-            message: "Cannot set drone to zone",
+            message: "Cannot set itinerary to zone",
             content: error.message
         })
     }
@@ -22,16 +22,16 @@ exports.setItinerarytoZone = async (req, res) => {
 exports.getItinerarybyZone = async (req, res) => {
     try {
         let _id = req.params._id;
-        let result = await droneService.getItinerarybyZone(_id);
+        let result = await itineraryService.getItinerarybyZone(_id);
         res.status(200).json({
             success: true,
-            message: "Get drone by zone successfully",
+            message: "Get itinerary by zone successfully",
             content: result
         })
     } catch (error){
         res.status(400).json({
             success: false, 
-            message: "Cannot get drone by zone",
+            message: "Cannot get itinerary by zone",
             content: error.message
         })
     }
@@ -40,17 +40,17 @@ exports.getItinerarybyZone = async (req, res) => {
 exports.deleteItinerarybyZone = async (req, res) => {
     try {
         const data = req.body
-        let result = await droneService.deleteItinerarytoZone(data);
+        let result = await itineraryService.deleteItinerarytoZone(data);
         res.status(200).json({
             success: true,
-            message: "delete drone by zone successfully",
+            message: "delete itinerary by zone successfully",
             content: result
         })
 
     }catch(error){
         res.status(400).json({
             success: false, 
-            message: "Cannot delete drone by zone",
+            message: "Cannot delete itinerary by zone",
             content: error.message
         })
     }
@@ -59,17 +59,17 @@ exports.deleteItinerarybyZone = async (req, res) => {
 exports.deleteItineraryTest = async (req, res) => {
     try {
         var _id = req.params._id
-        let result = await droneService.deleteItineraryTest(_id);
+        let result = await itineraryService.deleteItineraryTest(_id);
         res.status(200).json({
             success: true,
-            message: "delete drone by zone successfully",
+            message: "delete itinerary by zone successfully",
             content: result
         })
 
     }catch(error){
         res.status(400).json({
             success: false, 
-            message: "Cannot delete drone by zone test",
+            message: "Cannot delete itinerary by zone test",
             content: error.message
         })
     }
