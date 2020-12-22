c34998d2bc1437ec3eb3fec608a53700794ada1a
nganluu <nganltt712@gmail.com>
2020-12-22 23:36:54 +0700

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,11 +12,11 @@ module.exports = {
         max_memory_restart: '2G',
         env: {
             NODE_ENV: 'development',
-            PORT: 5011 
+            PORT: 5000 
         },
         env_production: {
             NODE_ENV: 'production',
-            PORT: 5011,
+            PORT: 5000,
             CI: false
         }
     }],
