b14826e21b08ea6a0c075764a0e90d0b4db155fe
nganluu <nganltt712@gmail.com>
2020-12-22 23:23:37 +0700

    add file ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    apps: [{
+        name: 'SDS 11',
+        script: 'index.js',
+
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem$
+        args: 'one',
+        instances: 1,
+        exec_mode: "cluster",
+        autorestart: true,
+        watch: true,
+        max_memory_restart: '2G',
+        env: {
+            NODE_ENV: 'development',
+            PORT: 5011 
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            PORT: 5011,
+            CI: false
+        }
+    }],
+
+    deploy: {
+        production: {
+            user: 'node',
+            host: '192.168.10.111',
+            ref: 'origin/master',
+            repo: 'git@github.com:repo.git',
+            path: '/var/www/production',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+        }
+    }
+};
